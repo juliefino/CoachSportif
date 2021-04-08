46203d0df2f8e5e89f1057334cc082699be6a588
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-04-08 09:34:56 +0200

    Nouvelle page Login

---
+++ frontend/src/components/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import FormSignin from "../FormSignIn"
+export default function SignIn() {
+  return (
+      <>
+        <FormSignin />;
+      </>
+
+  );
+}
