34585db148e1220cedfd5a26b1306004bf0d1e5d
juliefino <j.fino@students.ephec.be>
2021-04-09 13:55:22 +0200

    Refactoring code bouton trier

--- frontend/src/components/ButtonSort.js
+++ frontend/src/components/ButtonSort.js
@@ -1,29 +1,128 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Cards.css';
 import './ButtonSort.css';
 import { Link } from 'react-router-dom';
+import CardItem from "./CardItem";
 
+const Ascending = () => {
+    return <div className='cards'>
+                    <h1>ACTIVITÉS</h1>
 
-class ButtonSort extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {button_state : true};
-        this.manageState = this.manageState.bind(this);
-    }
-    manageState() {
-        this.setState(state => ({
-            button_state: !state.button_state
-        }))
-    }
-
-    render() {
-        return (
-            <div>
-                <button className='btn' onClick={this.manageState}>
-                    {this.state.button_state ? 'Trier par ordre croissant' : 'Trier par ordre décroissant'}
-                </button>
-             </div>
-        );
-    }
+                    <div className='cards__container'>
+                        <div className='cards__wrapper'>
+                            <ul className='cards__items'>
+                                <CardItem
+                                    src='images/atletismo.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Athletisme'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/basketball.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Basketball'
+                                    path='/activites'
+                                />
+                            </ul>
+                            <ul className='cards__items'>
+                                <CardItem
+                                    src='images/futbol.jpg'
+                                    label='Football'
+                                    text='Inscrivez-vous à cette activité'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/natacion.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Natation'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/tenis.png'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Tennis'
+                                    path='/activites'
+                                />
+                            </ul>
+                        </div>
+                    </div>
+                </div>;
 }
 
-export default ButtonSort
+const Descending = () => {
+    return <div className='cards'>
+                    <h1>ACTIVITÉS</h1>
+
+                    <div className='cards__container'>
+                        <div className='cards__wrapper'>
+                            <ul className='cards__items'>
+                                <CardItem
+                                    src='images/tenis.png'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Tennis'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/natacion.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Natation'
+                                    path='/activites'
+                                />
+                            </ul>
+                            <ul className='cards__items'>
+                                <CardItem
+                                    src='images/futbol.jpg'
+                                    label='Football'
+                                    text='Inscrivez-vous à cette activité'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/basketball.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Basketball'
+                                    path='/activites'
+                                />
+                                <CardItem
+                                    src='images/atletismo.jpg'
+                                    text='Inscrivez-vous à cette activité'
+                                    label='Athletisme'
+                                    path='/activites'
+                                />
+                            </ul>
+                        </div>
+                    </div>
+                </div>;
+}
+
+const ButtonSort = () => {
+
+    const [button, setButton] = useState(true);
+    const [ascendant, setAscendant] = useState("Ascendant");
+    const [descendant, setDescendant] = useState("Descendant");
+    let label = ["Ascendant", "Descendant"];
+    let show ="";
+
+    const handleClick = () => {
+        setButton(!button);
+        if(button === true) {
+            show = label[0];
+            setAscendant(ascendant);
+        }
+
+        else {
+            show = label[1];
+            setDescendant(descendant);
+
+        }
+    }
+
+        return (
+            <div>
+                <button className='btn' onClick={handleClick}>{button ? descendant : ascendant}</button>
+                    {button ? <Ascending />  : <Descending/>}
+             </div>
+        );
+}
+
+
+export default ButtonSort;
