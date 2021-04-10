6e3e318be4c4de46ab6c68cad93d9231f2c8f3fd
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-04-10 14:44:42 +0200

    Implémentation des Models pour Activites et Objectifs

--- model.py
+++ model.py
@@ -21,4 +21,37 @@ class Utilisateur(db.Model):
         self.poids = poids
         self.password = hashlib.md5(password.encode()).hexdigest() #faut pas laisser en 'claire' les password dans la DB !
 
+
+class Activites(db.Model):
+    __tablename__ = "activites"
+
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_activite = db.Column(db.String(200))
+
+
+
+class Activites_Likees(db.Model):
+    __tablename__ = "activites_likees"
+
+    id_user = db.Column(db.Integer, db.ForeignKey("utilisateurs.id"), primary_key=True)
+    id_activite = db.Column(db.Integer, db.ForeignKey("activites.id"), primary_key=True)
+
+
+
+class Objectifs(db.Model):
+    __tablename__ = 'objectifs'
+
+    id = db.Column(db.Integer, primary_key=True,  autoincrement=True)
+    nom_objectif = db.Column(db.String(200))
+
+
+
+class Objectifs_Utilisateurs(db.Model):
+    __tablename__ = "objectifs_utilisateurs"
+
+    id_user = db.Column(db.Integer, db.ForeignKey("utilisateurs.id"),  primary_key=True)
+    id_objectif = db.Column(db.Integer, db.ForeignKey("objectifs.id"),  primary_key=True)
+
+
 db.create_all()
