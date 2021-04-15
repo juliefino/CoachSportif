5eb220890f59f65ad2637c96690242b192d68f21
juliefino <j.fino@students.ephec.be>
2021-04-15 17:14:25 +0200

    Update formulaire contact

--- frontend/src/components/FormContact.js
+++ frontend/src/components/FormContact.js
@@ -1,7 +1,5 @@
 import './FormContact.css';
 import React, {useState} from "react";
-import {MdTextFields} from "@react-icons/all-files/md/MdTextFields";
-
 
 
 const FormSignup = () => {
@@ -16,54 +14,33 @@ const FormContact = () => {
     return (
     <div>
         <h1 className='title'>FORMULAIRE DE CONTACT</h1>
-        <form className="form" onSubmit={(event) => {
+        <form className="box" onSubmit={(event) => {
             /*send(event)*/
         }}>
-            <div className="inputs">
-                <label className='labels'>ADRESSE E-MAIL : <br></br></label>
-                <input
-                    className='input'
-                    type='email'
-                    required
-                    name='email'
-                    placeholder='Entrez votre adresse e-mail'
-                    /*value={email}
-                    onChange={(e) => setAlias(e.target.value)} */
-
-                />
+            <div className='pack'>
+                <label className='subtitles'>ADRESSE E-MAIL : </label>
+                <br></br>
+                <input className='entry' type='email' required
+                       name='email' placeholder='Entrez votre adresse e-mail'/>
             </div>
-            <div className="inputs">
-                <label className='labels'>SUJET : <br></br> </label>
-                <input
-                    className='input'
-                    type='text'
-                    required
-                    name='subject'
-                    placeholder='Sujet de la demande'
-                    /*value={subject}
-                    onChange={(e) => setTaille(e.target.value)}*/
-                />
-            </div>
-            <div className='inputs'>
-                <label className='labels'>DÉCRIVEZ VOTRE DEMANDE : <br></br> </label>
-                <MdTextFields
 
-                    id="outlined-multiline-static"
-                    multiline
-                    rows={6}
-                    defaultValue="Ecrivez votre demande ici"
-                    variant="outlined"
-
-                    className='input'
-                    required
-                    name='demande'
-                    /*value={demande}
-                    onChange={(e) => setPassword(e.target.value)}*/
-                />
+            <div className="pack">
+                <label className='subtitles'>SUJET : </label>
+                <br></br>
+                <input className='entry' type='text' required
+                       name='subject' placeholder='Sujet de la demande'/>
             </div>
-            <button type="submit" className='form-input-btn'>ENVOYER LA DEMANDE</button>
+
+            <div className='pack'>
+                <label className='subtitles'>DÉCRIVEZ VOTRE DEMANDE : </label>
+                <br></br>
+                <textarea placeholder="Ecrivez votre demande ici" className='textbox' required
+                          name='demande' maxLength= '300'>
+                </textarea>
+            </div>
+
+            <button type="submit" className='form-input-btn-contact'>ENVOYER LA DEMANDE</button>
         </form>
-
     </div>
     );
 }
