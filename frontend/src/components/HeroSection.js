74c2c2f7e92e0e08a9f2070f0672b6903efe00f4
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-04-15 18:08:27 +0200

    Modification du container

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import '../App.css';
 import { Button } from './Button';
+import {Link} from "react-router-dom";
 import './HeroSection.css';
-
+import {isLoggedIn} from './auth.js';
 function HeroSection() {
-  return (
-    <div className='hero-container'>
-      <img src='/images/img-02.jpg' />
-      <h1>WORK HARDER, GET STRONGER</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          S'INSCRIRE
-        </Button>
 
-      </div>
-    </div>
-  );
+    if(isLoggedIn()){
+        return (
+
+        <div className='hero-container'>
+          <img src='/images/img-02.jpg' />
+          <h1>WORK HARDER, GET STRONGER</h1>
+          <p>What are you waiting for?</p>
+
+        </div>)
+    }else{
+        return(
+        <div className='hero-container'>
+          <img src='/images/img-02.jpg' />
+          <h1>WORK HARDER, GET STRONGER</h1>
+          <p>What are you waiting for?</p>
+          <div className='hero-btns'>
+            <Button
+              className='form-input-btn'
+            ><Link to="/sign-up">
+              S'INSCRIRE</Link>
+            </Button>
+
+          </div>
+        </div>)
+    }
+
 }
 
 export default HeroSection;
