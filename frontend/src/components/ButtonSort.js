1ab2fa9af78967765755f16849ab14648dc9a359
Mathis Dory <mathis@air-de-mathis.home>
2021-04-16 16:15:45 +0200

    Rajout des sauvegardes des activités pour le bouton descending

--- frontend/src/components/ButtonSort.js
+++ frontend/src/components/ButtonSort.js
@@ -78,15 +78,24 @@ const Ascending = () => {
 }
 
 const Descending = () => {
-    const url = "/api/activites";
+    const url1 = "/api/activites";
+    const url2 = "/api/activitesLikees";
     const [items, SetItems] = useState(null);
     let content = null;
     useEffect(() => {
-            axios.get(url)
-                .then(response => {
-                    SetItems(response.data)
-                })
-        }, [url]
+            axios.all([
+                axios.get(url1),
+                axios.get(url2)
+            ])
+                .then(axios.spread((data1, data2) => {
+                     SetItems(data1.data);
+                     for (let i in data2.data) {
+                        if (data2.data[i].id_user === +localStorage.getItem('id')) {
+                            document.getElementById(data2.data[i].id_activity).childNodes[0].style.color = "red";
+                        }
+                    }
+                }));
+        }, [url1]
     )
     if (items) {
         content = <div className='cards__container'>
@@ -96,6 +105,7 @@ const Descending = () => {
                         src={items['5'].img}
                         text='Inscrivez-vous à cette activité'
                         label={items['5'].label}
+                        id={items['5'].id}
                         path='/activites'
                     />
                     <CardItem
@@ -103,6 +113,7 @@ const Descending = () => {
                         text='Inscrivez-vous à cette activité'
                         label={items['4'].label}
                         path='/activites'
+                        id={items['4'].id}
                     />
                 </ul>
                 <ul className='cards__items'>
@@ -111,18 +122,21 @@ const Descending = () => {
                         label={items['3'].label}
                         text='Inscrivez-vous à cette activité'
                         path='/activites'
+                        id={items['3'].id}
                     />
                     <CardItem
                         src={items['2'].img}
                         text='Inscrivez-vous à cette activité'
                         label={items['2'].label}
                         path='/activites'
+                        id={items['2'].id}
                     />
                     <CardItem
                         src={items['1'].img}
                         text='Inscrivez-vous à cette activité'
                         label={items['1'].label}
                         path='/activites'
+                        id={items['1'].id}
                     />
                 </ul>
             </div>
