9255746870c384522caeab2778dbf59133c6608b
Eliott Lepage <HE201841@students.ephec.be>
2021-04-16 16:02:36 +0200

    Changement des routes et du nom de fichier

--- frontend/src/components/pages/Encodage_distance.js
+++ frontend/src/components/pages/Encodage_distance.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import { Redirect } from 'react-router-dom';
 import '../../App.css'
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Col, Row, Form } from "react-bootstrap";
 
 export default function Distance() {
 
@@ -14,21 +15,23 @@ export default function Distance() {
   const [distance, setDistance] = useState('')
   const [duree, setDuree] = useState('')
   const [vitesse_moyenne, setVitesse_moyenne] = useState('')
+  const nothing = null
+
 
 const send = async (event) => {
     event.preventDefault();
 try{
     console.log("Enter fetch !")
-    let result =  await fetch('/api/encodage-distance', {
+    let result =  await fetch('/api/encodage', {
     method: 'POST',
     headers: {
         'Content-type': 'application/json',
         'accept':'application/json'
       },
-    body: JSON.stringify({id_user, id_activite, date, heure, distance, duree, vitesse_moyenne})
+    body: JSON.stringify({id_user, id_activite, date, heure, distance, duree, vitesse_moyenne,
+        nothing, nothing, nothing, nothing})
   })
     console.log("Done")
-    console.log(result)
     console.log(result.status) //pour afficher le bon status, il faut retirer le mode no-cors
 }
 
@@ -38,10 +41,10 @@ catch(e){
 }
 
    return (
-       <>   <h1 className='objectifs'>PERFORMANCE</h1>
+       <>   <h1>PERFORMANCE DISTANCE</h1>
             <div className='form-content'>
                 <form className="form" onSubmit={(event) => {send(event)}}>
-
+                <p className="introduction"> Veuillez introduire les différentes données correspondant à l'activité que vous avez réalisée dans les champs proposés</p>
                     <div className="form-inputs">
                     <label className='form-label'>Date </label>
                       <input className='form-input' type='date' required value={date} name="date"
@@ -67,8 +70,8 @@ catch(e){
                     </div>
 
                     <div className='form-inputs'>
-                       <label className='form-label'>Vitesse moyenne <bdi> minutes / km </bdi></label>
-                         <input required className='form-input' type="number" placeholder="1.0" step="0.01" min="0.01" value={vitesse_moyenne} name="vitesse_moyenne"
+                       <label className='form-label'>Vitesse moyenne <bdi> minutes : secondes / km </bdi></label>
+                         <input required className='form-input' type="time" value={vitesse_moyenne} name="vitesse_moyenne"
                             onChange={ (e) => setVitesse_moyenne(e.target.value) }/>
                     </div>
 
