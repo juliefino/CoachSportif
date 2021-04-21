de31fbf5435291e164400f5c622d4a0e6c8d165a
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-04-21 09:52:14 +0200

    Changement nom des classes

--- frontend/src/components/FormSignIn.js
+++ frontend/src/components/FormSignIn.js
@@ -1,4 +1,5 @@
 import React, {  useState } from 'react';
+import {Link} from "react-router-dom";
 import './Form.css';
 
 import { Redirect } from 'react-router-dom';
@@ -92,7 +93,11 @@ const FormSignin = () => {
                         </div>
 
                         <button onClick={handleClick} className='form-input-btn'>SE CONNECTER</button>
+                        <div className="connexion">
+                         <p className="conect">Vous n'avez pas de compte, alors <Link to='/sign-up' className="inscrire" style={{color: 'white'}}>INSCRIVEZ VOUS</Link></p>
+                        </div>
                     </form>
+
                      </div>
             </div>
    );
