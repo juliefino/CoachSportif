86acc510cae092e9541f118ad40afa53c7c76b08
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-05-01 13:50:49 +0200

    Pages données personnels

---
+++ frontend/src/components/pages/DonneesPersonnels.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import FormDonnees from "../FormDonnees"
+
+export default function DonneesPersonnels() {
+    return (
+        <>
+            <FormDonnees/>;
+        </>
+
+    );
+}
