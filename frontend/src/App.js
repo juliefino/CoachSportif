7822ef8675ed8a272c3b61a67f4eff39e563345e
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-05-03 21:09:47 +0200

    Elimination routes non nécessaire

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Partage from './components/pages/Services';
 import Activites from './components/pages/Activites';
 import FormSignup from './components/FormSignUp';
-import Success from "./components/pages/Success";
 import NotFound from "./components/pages/NotFound";
 import FormSignin from './components/FormSignIn';
 import Conditions from './components/pages/Conditions';
@@ -15,7 +14,6 @@ import Distance from './components/pages/Encodage_distance';
 import Score from './components/pages/Encodage_score';
 import Aquatique from './components/pages/Encodage_aquatique';
 import {isLoggedIn} from './components/auth.js';
-import Profile from "./components/pages/Profile";
 import { PrivateRoute } from "./components/PrivateRoute.js";
 import Contact from "./components/pages/Contact";
 import Sidebar from "./components/sidebar";
@@ -42,14 +40,12 @@ function App() {
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_distance" component={Distance} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_score" component={Score} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_aquatique" component={Aquatique} />
-          <PrivateRoute exact isloggedin={isLoggedIn()} path='/profile' component={Profile} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path='/nutrition' component={Nutrition} />
           <Route path='/partage' component={Partage} />
           <Route path='/activites' component={Activites} />
           <Route path='/sign-up' component={FormSignup} />
           <Route path='/login' component={FormSignin} />
           <Route path='/conditions' component={Conditions} />
-          <Route path='/success' component={Success} />
           <Route path='/contact' component={Contact} />
           <Route path='/premium' component={Premium} />
 
@@ -74,13 +70,11 @@ function App() {
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_distance" component={Distance} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_score" component={Score} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_aquatique" component={Aquatique} />
-          <PrivateRoute exact isloggedin={isLoggedIn()} path='/profile' component={Profile} />
           <Route path='/partage' component={Partage} />
           <Route path='/activites' component={Activites} />
           <Route path='/sign-up' component={FormSignup} />
           <Route path='/login' component={FormSignin} />
           <Route path='/conditions' component={Conditions} />
-          <Route path='/success' component={Success} />
           <Route path='/contact' component={Contact} />
           <Route path='/premium' component={Premium} />
 
@@ -101,13 +95,11 @@ function App() {
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_distance" component={Distance} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_score" component={Score} />
           <PrivateRoute exact isloggedin={isLoggedIn()} path="/encodage_aquatique" component={Aquatique} />
-          <PrivateRoute exact isloggedin={isLoggedIn()} path='/profile' component={Profile} />
           <Route path='/partage' component={Partage} />
           <Route path='/activites' component={Activites} />
           <Route path='/sign-up' component={FormSignup} />
           <Route path='/login' component={FormSignin} />
           <Route path='/conditions' component={Conditions} />
-          <Route path='/success' component={Success} />
           <Route path='/contact' component={Contact} />
           <Route path='/premium' component={Premium} />
 
