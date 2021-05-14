44ec02526942411b08a62f4153c8ce49a3d206db
Eliott Lepage <HE201841@students.ephec.be>
2021-05-14 16:26:48 +0200

    Ajout de tests sur les 3 pages encodages

--- frontend/src/components/pages/Encodages/Encodages.test.js
+++ frontend/src/components/pages/Encodages/Encodages.test.js
@@ -1,10 +1,41 @@
-import ReactDOM from "react-dom";
-import {MemoryRouter} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Distance from "./Encodage_distance";
-import SignIn from "./Connexion/SignIn";
-import React from "react";
+import Score from "./Encodage_score";
+import Aquatique from "./Encodage_aquatique";
+
+import TestRenderer from "react-test-renderer";
 
 test("render sans que ça plante", () => {
   const div = document.createElement("div");
   ReactDOM.render(<MemoryRouter><Distance /></MemoryRouter>, div);
+});
+
+test("render sans que ça plante", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<MemoryRouter><Score /></MemoryRouter>, div);
+});
+
+test("render sans que ça plante", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<MemoryRouter><Aquatique /></MemoryRouter>, div);
+});
+
+it("renders a form", () => {
+  const testRenderer = TestRenderer.create(<MemoryRouter><Distance /></MemoryRouter>);
+  const testInstance = testRenderer.toJSON();
+  expect(testInstance).toMatchSnapshot();
+});
+
+it("renders a form", () => {
+  const testRenderer = TestRenderer.create(<MemoryRouter><Score /></MemoryRouter>);
+  const testInstance = testRenderer.toJSON();
+  expect(testInstance).toMatchSnapshot();
+});
+
+it("renders a form", () => {
+  const testRenderer = TestRenderer.create(<MemoryRouter><Aquatique /></MemoryRouter>);
+  const testInstance = testRenderer.toJSON();
+  expect(testInstance).toMatchSnapshot();
 });
