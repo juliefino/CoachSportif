1b2a0fd7313a8d1c4a7cdead7dff1865056813a6
juliefino <j.fino@students.ephec.be>
2021-05-17 22:55:31 +0200

    Modif article fruits & legumes

--- frontend/src/components/pages/Nutrition/fruitLegumes.js
+++ frontend/src/components/pages/Nutrition/fruitLegumes.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react';
+import {motion} from "framer-motion";
 
 
 function FruitsLegumes () {
   return (
-    <h1>Hola</h1>
+   <div className='article'>
+          <div>
+          {/* Image*/}
+          </div>
+        <motion.h1 className='titre' animate={{fontSize: 50, color: '#4f3a3c'}}>LES FRUITS ET LEGUMES FRAIS</motion.h1>
+         <div className= 'contenu'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget dolor ut sapien bibendum congue. In ut eleifend nunc. Vivamus eu velit ac sem vehicula consectetur nec venenatis ante. Donec maximus, elit ac commodo iaculis, eros nunc dignissim purus, in vehicula est sapien non enim. <br/> <br/> Nullam quis justo vulputate, dapibus leo non, semper purus. Phasellus eget felis ligula. Sed iaculis ex eu leo posuere, et placerat purus volutpat.
+
+Ut rhoncus, est a tempor molestie, libero nisi molestie odio, sed rhoncus massa magna non dui. Morbi varius molestie lectus et hendrerit.<br/> <br/> Proin sed elit nunc. Pellentesque pulvinar feugiat volutpat. Pellentesque quis purus nec orci lacinia ullamcorper vel id elit. Mauris non erat ut lectus auctor posuere. Vivamus rhoncus tristique erat, a porttitor diam placerat eu.
+
+Nullam sit amet dui sit amet enim accumsan bibendum sed nec nunc. <br/>
+             <br/>
+             Curabitur id metus leo. Praesent magna nisi, viverra vel tincidunt id, elementum vitae nisl. Donec maximus tortor ipsum, vel ultricies est vulputate tincidunt. Proin in lacus non libero tristique tristique. Curabitur augue diam, vulputate id interdum ac, cursus sed ligula. Donec accumsan magna velit, non dapibus leo ultricies id. <br/> <br/> Maecenas urna dolor, posuere vitae mollis ut, venenatis vitae velit. Fusce aliquet mauris est, vel semper elit tempus nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Praesent felis magna, luctus ut fermentum eget, vehicula sit amet tellus. Nulla id quam et nibh sodales rutrum vitae at massa. Duis id convallis libero. Quisque consequat, mi et laoreet tincidunt, massa neque efficitur nisl, a sollicitudin nulla enim id libero. Aenean volutpat varius ex at posuere. Aliquam erat volutpat. Etiam porttitor nunc ultrices viverra pulvinar. Vivamus ac nunc ullamcorper, luctus arcu vitae, volutpat leo. Aenean nec rutrum dui. Etiam ultrices vitae erat quis rutrum. Quisque imperdiet leo in convallis tempus. <br/><br/> Vivamus non magna nec urna laoreet egestas. Integer dapibus ipsum euismod congue facilisis. Phasellus varius placerat nunc id laoreet. Curabitur ornare posuere nunc ac pharetra. Morbi condimentum ac eros quis tempus. Vivamus ut.
+         </div>
+    </div>
   );
 };
 export default FruitsLegumes;
