0661b763d77615c5e857444f0e24f660940521af
Mathis Dory <mathis@MacBook-Air-de-Mathis.local>
2021-05-18 20:11:59 +0200

    Modification de l'id des messages

--- backend/model.py
+++ backend/model.py
@@ -132,7 +132,7 @@ class Commandes(db.Model):
 class Messages(db.Model):
     __tablename__ = "messages"
 
-    id = db.Column(db.String(50), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_user = db.Column(db.Integer, db.ForeignKey("utilisateurs.id"))
     id_encodage = db.Column(db.Integer, db.ForeignKey("encodage.id_encodage"))
     message = db.Column(db.String(500))
