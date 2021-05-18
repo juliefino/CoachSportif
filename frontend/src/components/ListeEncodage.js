1ba2683da61bd93b282e5e0576537593148ee4ce
Mathis Dory <mathis@MacBook-Air-de-Mathis.local>
2021-05-18 16:20:25 +0200

    Suppresion du bouton partage, abandon de la US

--- frontend/src/components/ListeEncodage.js
+++ frontend/src/components/ListeEncodage.js
@@ -19,45 +19,46 @@ const ListeEncodage = () => {
                     }
                 }
                 for (let j = 0; j < arrayEncoded.length; j++) {
-                    if (arrayEncoded[j]['id_activite'] == 1) {
+                    if (arrayEncoded[j]['id_activite'] === 1) {
                         nom_activite = "Athlétisme";
-                        listEncoded_distance += "<tr><td>" + nom_activite + "</td>" + "<td>" + arrayEncoded[j]['date'] + "</td>" + "<td>" + arrayEncoded[j]['hour']
-                            + "</td>" + "<td>" + arrayEncoded[j]['distance'] + "</td>" + "<td>" + arrayEncoded[j]['time'] + "</td>" + "<td>" + arrayEncoded[j]['average_speed'] + "</td>" +
-                             "<td><Link to='/partage'><button>Partager</button></Link></td></tr>";
+                        listEncoded_distance += "<tr><td>" + nom_activite + "</td><td>" + arrayEncoded[j]['date'] + "</td><td>" + arrayEncoded[j]['hour']
+                            + "</td><td>" + arrayEncoded[j]['distance'] + "</td><td>" + arrayEncoded[j]['time'] + "</td><td>" + arrayEncoded[j]['average_speed'] + "</td></tr>";
                     }
 
-                    else if (arrayEncoded[j]['id_activite'] == 4) {
+
+                    else if (arrayEncoded[j]['id_activite'] === 4) {
                         nom_activite = "Natation";
-                        listEncoded_aquatique += "<tr><td>" + nom_activite + "</td>" + "<td>" + arrayEncoded[j]['date'] + "</td>" + "<td>" + arrayEncoded[j]['distance']
-                        + "</td>" + "<td>" + arrayEncoded[j]['time'] + "</td>" + "<td><Link to='/partage'><button>Partager</button></Link></td></tr>";
+                        listEncoded_aquatique += "<tr><td>" + nom_activite + "</td><td>" + arrayEncoded[j]['date'] + "</td><td>" + arrayEncoded[j]['distance']
+                        + "</td><td>" + arrayEncoded[j]['time'] + "</td></tr>";
                     }
 
-                    else if (arrayEncoded[j]['id_activite'] == 2 || 3 || 5) {
-                        if (arrayEncoded[j]['id_activite'] == 2) {
+                    else if (arrayEncoded[j]['id_activite'] === 2 || 3 || 5) {
+                        if (arrayEncoded[j]['id_activite'] === 2) {
                             nom_activite = "Basketball";
                         }
-                        else if (arrayEncoded[j]['id_activite'] == 3) {
+                        else if (arrayEncoded[j]['id_activite'] === 3) {
                             nom_activite = "Football";
                         }
-                        else if (arrayEncoded[j]['id_activite'] == 5) {
+                        else if (arrayEncoded[j]['id_activite'] === 5) {
                             nom_activite = "Tennis";
                         }
 
-                        listEncoded_score += "<tr><td>" + nom_activite + "</td>" + "<td>" + arrayEncoded[j]['date'] + "</td>" + "<td>" + arrayEncoded[j]['time'] + "</td>" + "<td>" + arrayEncoded[j]['team1'] + "</td>" + "<td>"
-                        + arrayEncoded[j]['team2'] + "</td>" + "<td>" + arrayEncoded[j]['score1'] + "</td>" + "<td>"
-                        + arrayEncoded[j]['score2'] + "</td>" + "<td><Link to='/partage'><button>Partager</button></Link></td></tr>";
+                        listEncoded_score += "<tr><td>" + nom_activite + "</td><td>" + arrayEncoded[j]['date'] + "</td><td>" + arrayEncoded[j]['time'] + "</td><td>" + arrayEncoded[j]['team1'] + "</td><td>"
+                        + arrayEncoded[j]['team2'] + "</td><td>" + arrayEncoded[j]['score1'] + "</td><td>"
+                        + arrayEncoded[j]['score2'] + "</td></tr>";
                     }
 
                     else {
                         console.log("Error");
                     }
+
                 }
                 document.getElementById("tableEncoded_distance").innerHTML = listEncoded_distance;
                 document.getElementById("tableEncoded_score").innerHTML = listEncoded_score;
                 document.getElementById("tableEncoded_aquatique").innerHTML = listEncoded_aquatique;
             }
         )
-    })
+    },[])
 
     return (
         <div className="container-list-encoded">
